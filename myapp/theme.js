4e0559a45e730aeec4f22faa423ee324c38a2983
pkasiedu <pkasiedu007@st.ug.edu.gh>
2024-06-26 22:41:24 +0000

    first commit

---
+++ myapp/theme.js
@@ -0,0 +1,14 @@
+export const lightTheme = {
+    background: '#FFFFFF',
+    text: '#000000',
+    cardBackground: '#0D1F3C',
+    // Add more light theme styles as needed
+  };
+  
+  export const darkTheme = {
+    background: '#000000',
+    text: '#FFFFFF',
+    cardBackground: '#0D1F3C',
+    // Add more dark theme styles as needed
+  };
+  
